7dd74a80c66e12894fb7ccb66a6459d6985a081e
Novo <eunovo9@gmail.com>
2024-09-12 18:07:58 +0100

    .

--- server/tests/e2e_test.ts
+++ server/tests/e2e_test.ts
@@ -63,18 +63,18 @@ test.serial("End-to-end test", async (t) => {
 
     // Define supported currency pairs
     const currencyPairs = [
-        // { from: 'GHS', to: 'USDC' },
-        // { from: 'NGN', to: 'KES' },
-        // { from: 'KES', to: 'USD' },
-        // { from: 'USD', to: 'KES' },
+        { from: 'GHS', to: 'USDC' },
+        { from: 'NGN', to: 'KES' },
+        { from: 'KES', to: 'USD' },
+        { from: 'USD', to: 'KES' },
         { from: 'USD', to: 'EUR' },
         { from: 'EUR', to: 'USD' },
-        // { from: 'USD', to: 'GBP' },
-        // { from: 'USD', to: 'BTC' },
-        // { from: 'EUR', to: 'USDC' },
-        // { from: 'EUR', to: 'GBP' },
-        // { from: 'USD', to: 'AUD' },
-        // { from: 'USD', to: 'MXN' }
+        { from: 'USD', to: 'GBP' },
+        { from: 'USD', to: 'BTC' },
+        { from: 'EUR', to: 'USDC' },
+        { from: 'EUR', to: 'GBP' },
+        { from: 'USD', to: 'AUD' },
+        { from: 'USD', to: 'MXN' }
     ];
 
     // Perform transfers from bank accounts to user wallets
